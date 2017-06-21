e4dcaa20f0529f7ecde041ef2954f36b3408f62b
nickyzj <reg.zj@outlook.com>
2017-06-21 15:56:04 +0800

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { Routes, RouterModule } from '@angular/router';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { 
+  MdCheckboxModule,
+  MdRadioModule,
+  MdInputModule,
+  MdDatepickerModule,
+  MdNativeDateModule,
+  MdAutocompleteModule
+} from '@angular/material';
+
 import { AppComponent } from './app.component';
-import { GetStartedComponent } from './get-started/get-started.component';
-import { GridBasicComponent } from './grid-basic/grid-basic.component';
-import { TypographyComponent } from './typography/typography.component';
-import { TablesComponent } from './tables/tables.component';
-import { ImagesComponent } from './images/images.component';
-import { JumbotronComponent } from './jumbotron/jumbotron.component';
-import { WellsComponent } from './wells/wells.component';
-import { AlertsComponent } from './alerts/alerts.component';
-import { AnimationsComponent } from './animations/animations.component';
-import { ButtonsComponent } from './buttons/buttons.component';
-import { ButtonGroupsComponent } from './button-groups/button-groups.component';
-import { DropdownDirective } from './shared/dropdown.directive';
-import { GlyphiconsComponent } from './glyphicons/glyphicons.component';
-import { BadgesAndLabelsComponent } from './badges-and-labels/badges-and-labels.component';
-import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { GetStartedComponent } from './bootstrap/get-started/get-started.component';
+import { GridBasicComponent } from './bootstrap/grid-basic/grid-basic.component';
+import { TypographyComponent } from './bootstrap/typography/typography.component';
+import { TablesComponent } from './bootstrap/tables/tables.component';
+import { ImagesComponent } from './bootstrap/images/images.component';
+import { JumbotronComponent } from './bootstrap/jumbotron/jumbotron.component';
+import { WellsComponent } from './bootstrap/wells/wells.component';
+import { AlertsComponent } from './bootstrap/alerts/alerts.component';
+import { AnimationsComponent } from './bootstrap/animations/animations.component';
+import { ButtonsComponent } from './bootstrap/buttons/buttons.component';
+import { ButtonGroupsComponent } from './bootstrap/button-groups/button-groups.component';
+import { DropdownDirective } from './bootstrap/shared/dropdown.directive';
+import { GlyphiconsComponent } from './bootstrap/glyphicons/glyphicons.component';
+import { BadgesAndLabelsComponent } from './bootstrap/badges-and-labels/badges-and-labels.component';
+import { ProgressBarComponent } from './bootstrap/progress-bar/progress-bar.component';
+import { CheckboxComponent } from './material/checkbox/checkbox.component';
+import { DatepickerComponent } from './material/datepicker/datepicker.component';
+import { AutocompleteComponent } from './material/autocomplete/autocomplete.component';
 
 const appRoutes: Routes = [
   { path: '', component: GetStartedComponent },
-  { path: 'get-started', component: GetStartedComponent },
-  { path: 'grid-basic', component: GridBasicComponent },
-  { path: 'typography', component: TypographyComponent },
-  { path: 'tables', component: TablesComponent },
-  { path: 'images', component: ImagesComponent },
-  { path: 'jumbotron', component: JumbotronComponent },
-  { path: 'wells', component: WellsComponent },
-  { path: 'alerts', component: AlertsComponent },
-  { path: 'animations', component: AnimationsComponent },
-  { path: 'buttons', component: ButtonsComponent },
-  { path: 'button-groups', component: ButtonGroupsComponent },
-  { path: 'glyphicons', component: GlyphiconsComponent },
-  { path: 'badges-and-labels', component: BadgesAndLabelsComponent },
-  { path: 'progress-bar', component: ProgressBarComponent },
+  { path: 'bootstrap/get-started', component: GetStartedComponent },
+  { path: 'bootstrap/grid-basic', component: GridBasicComponent },
+  { path: 'bootstrap/typography', component: TypographyComponent },
+  { path: 'bootstrap/tables', component: TablesComponent },
+  { path: 'bootstrap/images', component: ImagesComponent },
+  { path: 'bootstrap/jumbotron', component: JumbotronComponent },
+  { path: 'bootstrap/wells', component: WellsComponent },
+  { path: 'bootstrap/alerts', component: AlertsComponent },
+  { path: 'bootstrap/animations', component: AnimationsComponent },
+  { path: 'bootstrap/buttons', component: ButtonsComponent },
+  { path: 'bootstrap/button-groups', component: ButtonGroupsComponent },
+  { path: 'bootstrap/glyphicons', component: GlyphiconsComponent },
+  { path: 'bootstrap/badges-and-labels', component: BadgesAndLabelsComponent },
+  { path: 'bootstrap/progress-bar', component: ProgressBarComponent },
+  { path: 'material/checkbox', component: CheckboxComponent },
+  { path: 'material/datepicker', component: DatepickerComponent },
+  { path: 'material/autocomplete', component: AutocompleteComponent },
 ];
 
 @NgModule({
@@ -56,13 +73,24 @@ const appRoutes: Routes = [
     DropdownDirective,
     GlyphiconsComponent,
     BadgesAndLabelsComponent,
-    ProgressBarComponent
+    ProgressBarComponent,
+    CheckboxComponent,
+    DatepickerComponent,
+    AutocompleteComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MdCheckboxModule,
+    MdRadioModule,
+    MdInputModule,
+    MdDatepickerModule,
+    MdNativeDateModule,
+    MdAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/bootstrap/progress-bar/progress-bar.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'app-progress-bar',
+  templateUrl: 'progress-bar.component.html',
+  styleUrls: ['progress-bar.component.css'],
+})
+export class ProgressBarComponent {
+  
+}
+
+//https://material.angular.io/components/progress-bar/examples
